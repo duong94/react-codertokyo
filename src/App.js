cf54ce0417661552f6f292ea3d35e484dedde808
Admin <241538nxdd@gmail.com>
2020-07-10 20:36:08 +0700

    Bai 13 handleing

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import TodoItem from './components/TodoItem';
 class App extends Component {
   constructor() {
     super();
-    this.todoItems = [
+    this.state.todoItems = [
       {title: 'Mua bim bim', isComplete: true},
       {title: 'Đi đá bóng'},
       {title: 'Đi đổ xăng'}
     ]; 
   }
+
+OnItemClicked() {
+
+}
+
   render() {
     return <div className="App">
         {this.todoItems.length > 0 && this.todoItems.map((item, index) => (
-            <TodoItem key={index} item={item} />
+            <TodoItem key={index} item={item} onClick={this.onItemClicked} />
         ))}
       {this.todoItems.length === 0 && 'Nothing here'}
       </div>;
